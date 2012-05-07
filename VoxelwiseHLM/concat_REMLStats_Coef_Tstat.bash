b68dda635c2576ba7886b4cca52d54a70dd08f00
lncd <foranw@upmc.edu>
2012-05-07 12:19:28 -0400

    work from data files instead of internal conditions

--- VoxelwiseHLM/concat_REMLStats_Coef_Tstat.bash
+++ VoxelwiseHLM/concat_REMLStats_Coef_Tstat.bash
@@ -8,51 +8,43 @@
 #			There should be 312 visits bc I excluded 10816/111109163617
 
 #what do we want  e.g. ASerrorCorr ASCorr
-AStype="ASerrorCorr"
+#AStype="ASerrorCorr"
 
+#for AStype in "ASerrorCorr" "AScorr"; do
+for AStype in  "AScorr"; do
+   # for each of the subbricks desired
+   #for subb in "${AStype}#0_Coef" "${AStype}#0_Tstat"; do
+   for subb in "${AStype}#0_Coef"; do
+   #could do it by num #for subb in 2 3;
 
-# for each of the subbricks desired
-#for subb in "${AStype}#0_Coef" "${AStype}#0_Tstat"; do
-for subb in "${AStype}#0_Coef"; do
-#could do it by num #for subb in 2 3;
+      # outname is the type and either Coef or Tstat (*#0_ is stripted from var val)
+      outname="inputnii/$AStype-${subb#*#0_}"
 
-   # outname is the type and either Coef or Tstat (*#0_ is stripted from var val)
-   outname=$AStype-${subb#*#0_}
+      # what are we working on
+      echo "$AStype[$sub] to $outname"
 
-   # Tcat all REMLs 
-   3dTcat -overwrite -prefix $outname \
-         $(for reml in /Volumes/Governator/ANTISTATELONG/*/*/analysis/glm_hrf_Stats_REML.nii.gz; do
-              ## skip over unwanted
-              # always skip test and bad visit
-              if [[ "$reml" =~ "99999"        ]]; then  continue; fi
-              if [[ "$reml" =~ "111109163617" ]]; then  continue; fi
+      # Tcat all REMLs 
+      3dTcat -overwrite -prefix $outname \
+            $( 
+             # read in from the data file
+             awk '(NR>1){print $2"/*"$1, $80}' Data302_9to26_20120504_copy.dat |
+              while read path dA10se3sd; do
+                 # skip errors > 3std devs from mean in ROI
+                 [[ $AStype == "ASerrorCorr" && -z "$dA10se3sd" ]] && echo skipping $path 1>&2 && continue
 
-              # skip these if pulling ASerrorCorr
-              if [[ $AStype == "ASerrorCorr" ]]; then
-                 if [[ "$reml" =~ "10177/051117170743"  || 
-                       "$reml" =~ "10189/060207154918"  ||
-                       "$reml" =~ "10180/060306160512"  ||
-                       "$reml" =~ "10357/060907162211"  ||
-                       "$reml" =~ "10129/070811094021"  ||
-                       "$reml" =~ "10256/080625150930"  ||
-                       "$reml" =~ "10161/081023170012"  ||
-                       "$reml" =~ "10406/090611143538"  ||
-                       "$reml" =~ "10359/101122153412"  ||
-                       "$reml" =~ "10408/101201164822"  ||
-                       "$reml" =~ "10406/110314170503"  ]]; then 
-                          continue; 
-                fi
-              fi
+                 # should be a reml file here
+                 reml=$(ls -1 /Volumes/Governator/ANTISTATELONG/$path/analysis/glm_hrf_Stats_REML.nii.gz)
 
-              # include this subbrick in tcat
-              echo -n "$reml[$subb] " 
+                 # complain if there isnt
+                 [ ! -r $reml ] && echo "CANNOT READ reml in $reml" 1>&2 && continue
 
-            done)
-   fslhd $outname| grep '^dim'
+                 # add to the list fed into 3dTcat
+                 echo -n "$reml[$subb] " 
+             done)
+
+      # check dimensions
+      fslhd $outname| grep '^dim'
+   done
 done
-
 #3dbucket -overwrite -prefix allsubjects.nii.gz ASCorr-2.nii[1]  ASCorr-3.nii[1]
 
-#I renamed:
-#temp-2.nii --> AScorrBeta.nii
-#temp-3.nii --> AScorrTstat.nii
