9c725ab3ae0c24feff1a8dcf8d5446abee8cd047
Will Foran <foranw@upmc.edu>
2012-10-18 21:14:43 -0400

    plot sup fig 3 and 5 with R

---
+++ ROIHLM/plotData.R
@@ -0,0 +1,109 @@
+# source me and I'll create:
+# img_R20121018/
+#   supfig3-Left_PPC.png      supfig3-Right_putamen.png  supfig5-Left_dlPFC.png
+#   supfig3-Left_putamen.png  supfig3-SEF.png            supfig5-Left_vlPFC.png
+#   supfig5-Right_dlPFC.png
+
+library(ggplot2)
+library(reshape2)
+
+#AgeC <-c(-7.726,-6.726,-5.726,-4.726,-3.726,-2.726,-1.726,-0.726,0.274,1.274,2.274,3.274,4.274,5.274,6.274,7.274,8.274 );
+Age <- 9:25
+AgeC <- Age - 16.726
+# figure 3.
+files<- c( 'csv/ageC_SEF.csv', 'csv/ageC_PPC_L.csv', 'csv/ageC_putamen_L.csv', 'csv/ageC_putamen_R.csv')
+
+for (i in 1:length(files)){
+
+ # strip junk, get what the title should be
+ t<-sub('.*ageC_(.*).csv',"\\1", files[i], perl=T)
+ t<-sub('(.*)_L','Left \\1',t); t<-sub('(.*)_R','Right \\1',t)
+ print(paste(i,files[i],t))
+ # read in the csv
+ d<-read.table(files[i],sep=',',header=T)
+ # always use uppercase
+ names(d)<-toupper(names(d))
+ # make naming consistant
+ names(d) <- gsub("SEXMREF","SEXNUM",names(d)) 
+
+ ##
+ # make a dataframe with points for each age with  sex (color) and id (group) values
+ ##
+ # get the mean
+ m<-data.frame(y=sapply(AgeC, function(el){ d[1,'FVINTRCP'] + el*d[1,'FVAGEC']}), Age=Age, sex=3,id='mean')
+ # get individuals
+ d<-do.call("rbind",lapply(1:nrow(d),function(i){data.frame(y=sapply(AgeC, function(el){ d[i,'ECINTRCP'] + el*d[i,'ECAGEC']}), Age=Age, sex=d[i,'SEXNUM'],id=d[i,1])}))
+ # cobmine the two
+ d<-rbind(d,m)
+ # make sex a factor (attach mean to sex for displaying) and give names
+ d$sex<-as.factor(d$sex)
+ levels(d$sex)<-c('female','male','mean')
+
+ # setup plot
+ g <- ggplot(data=d,aes(x=Age,y=y,color=sex,size=sex))
+
+ png(paste('img_R20121018/supfig3-',sub(' ','_',t),'.png',sep=''))
+ print(
+   g + geom_line(aes(group=id)) + scale_color_manual(values=c('red','blue','black')) + scale_size_manual(values=(c(.5,.5,2)))+
+     ggtitle(t) + theme(text=element_text(size=24))+ scale_y_continuous('% Signal Change', limits=c(-.05,.15)) +
+     theme(legend.position="none")  #remove this to get the legend back
+ )
+ dev.off()
+}
+
+
+########################### parallels
+
+invAgeC     = c(0.051,0.040,0.031,0.024,0.017,0.012,0.007,0.003,-0.001,-0.004,-0.007,-0.010,-0.012,-0.014,-0.016,-0.018,-0.020);
+files <- paste('csv_parallel',c('dlPFC_L_invageC.csv' ,  'dlPFC_R_invageC.csv',  'vlPFC_L_invageC.csv'),sep='/');
+# problem file, merged sex55 with another file
+files <- sub('(vlPFC_L)','20121018_\\1',files)
+
+for (i in 1:length(files)){
+ print(paste(i,files[i],t))
+ # strip junk, get what the title should be
+ t<-sub('.*/(.*)_invageC.csv',"\\1", files[i], perl=T)
+ t<-sub('(.*)_L','Left \\1',t); t<-sub('(.*)_R','Right \\1',t); t<-sub('20121018_','',t)
+ # read in the csv
+ d<-read.table(files[i],sep=',',header=T)
+ # always use uppercase
+ names(d)<-toupper(names(d))
+ # make naming consistant
+ names(d) <- gsub("SEXMREF","SEXNUM",names(d))
+
+ # use sex55 if it exists
+ if(length(d$SEX55)>0)   d$SEXNUM<-d$SEX55+.5
+
+ ##
+ # make a dataframe with points for each age with  sex (color) and id (group) values
+ ##
+ # get the mean, make a full line
+ m<-data.frame(y=d[1,'FVINTRCP'] + invAgeC*d[1,'FVINVAGE'],
+          Age=Age, sex=3,id='mean')
+
+ # get individuals, only make a partial line
+ #agepartial=c(-.3,.3)
+
+ # the x in y=a*x+b is x=1/age - 1/mu = agepartial = 1/{16,17} -1/16.72
+ # so age should be 16,17
+ s<-do.call("rbind",lapply(1:nrow(d),function(i){data.frame(
+          y=d[i,'ECINTRCP'] + invAgeC[c(8,9)]*d[i,'ECINVAGE'],
+          Age=Age[c(8,9)],                 sex=d[i,'SEXNUM'],id=d[i,1])}))
+ # cobmine the two
+ tograph <-rbind(s,m)
+ # make sex a factor (attach mean to sex for displaying) and give names
+ tograph$sex<-as.factor(tograph$sex)
+ levels(tograph$sex)<-c('female','male','mean')
+
+ # setup plot
+ g <- ggplot(data=tograph,aes(x=Age,y=y,color=sex,size=sex))
+
+
+ png(paste('img_R20121018/supfig5-',sub(' ','_',t),'.png',sep=''))
+ print(g + geom_line(aes(group=id)) + scale_color_manual(values=c('red','blue','black')) + scale_size_manual(values=(c(.5,.5,2)))+
+     ggtitle(t) + theme(text=element_text(size=24))+ scale_y_continuous('% Signal Change', limits=c(-.05,.15)) +
+     theme(legend.position="none")  #remove this to get the legend back
+  )
+ dev.off()
+
+}
